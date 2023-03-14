6260765be1a5b5dd99bbce0ce1848c5f4d898070
DDane <82321835+DD-2703@users.noreply.github.com>
2023-03-14 11:49:14 +0530

    Package loading style changed, major changes

---
+++ exceptions.py
@@ -0,0 +1,14 @@
+class CrispConnectionError(ConnectionError):
+    pass
+
+
+class CrispNotImplementedError(NotImplementedError):
+    pass
+
+
+class CrispUnknownCommandError(ValueError):
+    pass
+
+
+class CrispUnknownPackageError(ValueError):
+    pass
